08fb462fc65f74db4233ba38f1d081d559b13acb
MiXaiLL76 <m.stepanov@redmadrobot.com>
2022-09-05 16:59:29 +0300

    move build curve to another function

--- faster_coco_eval/curves.py
+++ faster_coco_eval/curves.py
@@ -90,7 +90,40 @@ class Curves():
         i = np.where(mrec[1:] != mrec[:-1])[0]
         return np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
 
-    def plot_pre_rec(self, plotly_backend=False, label="category_id"):
+    def build_curve(self, label):
+        curve = []
+
+        if self.useCats:
+            cat_ids = list(range(self.eval['precision'].shape[2]))
+        else:
+            cat_ids = [0]
+
+        for category_id in cat_ids:
+            _label = f"[{label}={category_id}] "
+            if len(cat_ids) == 1:
+                _label = ""
+
+            precision_list = self.eval['precision'][:,:, category_id, :, :].ravel()
+            recall_list = self.recThrs
+            scores = self.eval['scores'][:, :, category_id, :, :].ravel()
+            auc = round(self.calc_auc(recall_list, precision_list), 4)
+
+            curve.append(dict(
+                recall_list=recall_list,
+                precision_list=precision_list,
+                name=f'{_label}auc: {auc:.3f}',
+                scores=scores,
+                auc=auc,
+                category_id=category_id,
+            ))
+        
+        return curve
+        
+
+    def plot_pre_rec(self, curves=None, plotly_backend=False, label="category_id"):
+        if curves is None:
+            curves = self.build_curve(label)
+
         use_plotly = False
         if plotly_backend:
             if plotly_available:
@@ -105,40 +138,25 @@ class Curves():
             fig.set_size_inches(15, 7)
             axes = [axes]
 
-        output = {
-            'auc': [],
-        }
 
-        if self.useCats:
-            cat_ids = list(range(self.eval['precision'].shape[2]))
-        else:
-            cat_ids = [0]
-
-        for category_id in cat_ids:
-            _label = f"[{label}={category_id}] "
-            if len(cat_ids) == 1:
-                _label = ""
-
-            precision_list = self.eval['precision'][:,
-                                                    :, category_id, :, :].ravel()
-            recall_list = self.recThrs
-
-            scores = self.eval['scores'][:, :, category_id, :, :].ravel()
-
-            auc = round(self.calc_auc(recall_list, precision_list), 4)
-
+        for _curve in curves:
+            recall_list = _curve['recall_list']
+            precision_list = _curve['precision_list']
+            scores = _curve['scores']
+            name = _curve['name']
+            
             if use_plotly:
                 fig.add_trace(
                     go.Scatter(
                         x=recall_list,
                         y=precision_list,
-                        name=f'{_label}auc: {auc:.3f}',
-                        mode='lines',
+                        name=name,
                         text=scores,
                         hovertemplate='Pre: %{y:.3f}<br>' +
                         'Rec: %{x:.3f}<br>' +
                         'Score: %{text:.3f}<extra></extra>',
                         showlegend=True,
+                        mode='lines',
                     ),
                     row=1, col=1
                 )
@@ -146,8 +164,7 @@ class Curves():
                 axes[0].set_title('Precision-Recall')
                 axes[0].set_xlabel('Recall')
                 axes[0].set_ylabel('Precision')
-                axes[0].plot(recall_list, precision_list,
-                             label=f'{_label}auc: {auc:.3f}')
+                axes[0].plot(recall_list, precision_list, label=name)
                 axes[0].grid(True)
                 axes[0].legend()
 
