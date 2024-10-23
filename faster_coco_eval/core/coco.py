f0304bc8a84e262d61884ca45f833e32208ff8c7
MiXaiLL76 <mike.milos@yandex.ru>
2024-10-23 19:52:54 +0300

    fix as pycocotools

--- faster_coco_eval/core/coco.py
+++ faster_coco_eval/core/coco.py
@@ -319,6 +319,7 @@ class COCO:
             res (obj)         : result api object
         """
         res = COCO()
+        res.dataset['info'] = copy.deepcopy(self.dataset['info'])
         res.dataset["images"] = [img for img in self.dataset["images"]]
 
         self.print_function("Loading and preparing results...")
