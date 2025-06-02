0fba1609ff392460a37c73a1d7624fe6dd808565
MiXaiLL76 <mike.milos@yandex.ru>
2025-06-02 17:41:14 +0300

    format docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,23 +4,19 @@ from datetime import datetime
 
 from faster_coco_eval import __author__, __version__
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath(".."))
 
-project = f'faster-coco-eval {__version__}'
+project = f"faster-coco-eval {__version__}"
 current_year = datetime.now().year
-copyright = f'2024-{current_year}, {__author__}'
+copyright = f"2024-{current_year}, {__author__}"
 author = __author__
 release = __version__
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",
-    "sphinx_autodoc_typehints",
-    "nbsphinx"
-]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_autodoc_typehints", "nbsphinx"]
 
 exclude_patterns = []
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+
 
 def setup(app):
-    app.add_css_file('custom_nbsphinx.css')
+    app.add_css_file("custom_nbsphinx.css")
